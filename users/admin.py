7d8a03939b3748248297dd3b21cc3fc695fcf945
Lilith <bichakhchyanlilith1992@gmail.com>
2026-06-13 21:30:28 +0300

    feat: Register User in admin for SuperUser

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from users.models import User
+
+
 # Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """
+    Администрирование модели Пользователь(User).
+    Супер позволяет управлять пользователями и их активностью.
+    """
+
+    list_display = ("id", "full_name", "email", "phone_number", "avatar", "address", "is_active")
