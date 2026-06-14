c166a68daa6f43d5da1ba83476ad9508d21a9423
Lilith <bichakhchyanlilith1992@gmail.com>
2026-06-14 17:24:54 +0300

    feat(auth): implement user registration workflow

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,23 @@
 from django.urls import path
 from rest_framework.permissions import AllowAny
-from rest_framework_simplejwt.views import (TokenObtainPairView,
-                                            TokenRefreshView)
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
+from users.views import UserRegisterAPIView, VerifyEmailView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path("login/", TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name="login"),
-    path("token/refresh/", TokenRefreshView.as_view(permission_classes=(AllowAny,)), name="token_refresh"),
+    path(
+        "login/",
+        TokenObtainPairView.as_view(permission_classes=(AllowAny,)),
+        name="login",
+    ),
+    path(
+        "token/refresh/",
+        TokenRefreshView.as_view(permission_classes=(AllowAny,)),
+        name="token_refresh",
+    ),
+    path("register/", UserRegisterAPIView.as_view(), name="register"),
+    path("verify/<str:token>/", VerifyEmailView.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+import secrets
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from users.models import User
+from users.serializers import UserSerializer
+from users.services import EmailVerification
+
+
+class UserRegisterAPIView(CreateAPIView):
+    """Регистрация пользователя с верификацией почтой"""
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=False)
+        token = secrets.token_hex(16)
+        user.verification_token = token
+        user.set_password(user.password)
+        user.save()
+        request = self.request
+        EmailVerification.authentication_by_email(
+            request=request, token=token, user_email=user.email
+        )
+
+
+class VerifyEmailView(APIView):
+    """Проверяет адрес электронной почты, связанный с предоставленным ключом.
+    Принимает следующий GET-параметр: key (token): Уникальный ключ активации/проверки,
+    отправляемый на электронную почту пользователя.
+    Возвращает:
+    При успешном выполнении (HTTP 200): {"message": "Email verified successfully. You can now log in."}.
+    При неудачном выполнении (HTTP 400): {"error": "Invalid verification token."}
+    """
+
+    permission_classes = (AllowAny,)
+
+    def get(self, request, token, *args, **kwargs):
+        try:
+            user = User.objects.filter(verification_token=token).first()
+            if user:
+                user.is_active = True
+                user.verification_token = None
+                user.save()
+            return Response(
+                {"message": "Email verified successfully. You can now log in."},
+                status=status.HTTP_200_OK,
+            )
+        except User.DoesNotExist:
+            return Response(
+                {"error": "Invalid verification token."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
