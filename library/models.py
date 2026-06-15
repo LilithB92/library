233332ff79a73b204b221f7f7eb53ab1d4a239a8
Lilith <bichakhchyanlilith1992@gmail.com>
2026-06-15 13:17:55 +0300

    docs(model.py): Added model Author for library app

--- library/models.py
+++ library/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django_countries.fields import CountryField
 
-# Create your models here.
+
+class Author(models.Model):
+    full_name = models.CharField(max_length=100, unique=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+    date_of_death = models.DateField(blank=True, null=True)
+    nationality = CountryField(
+        blank=True, null=True, blank_label="(Select nationality)"
+    )
+
+    def __str__(self):
+        return f"{self.full_name} ({self.nationality.name})"
+
+    class Meta:
+        verbose_name = "Автор"
+        verbose_name_plural = "Авторы"
