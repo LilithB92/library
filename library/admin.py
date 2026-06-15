0137b07a1ec8c41bb66f148755487595868e90f7
Lilith <bichakhchyanlilith1992@gmail.com>
2026-06-15 20:19:07 +0300

    feat: Register Author in administration panel for SuperUser

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from library.models import Author
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    """
+    Администрирование модели Пользователь(User).
+    Супер позволяет управлять пользователями и их активностью.
+    """
+
+    list_display = (
+        "pk",
+        "full_name",
+        "date_of_birth",
+        "date_of_death",
+        "nationality",
+    )
