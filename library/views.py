34b3d9f0f82c1c9d1574fdd088254366a8007599
Lilith <bichakhchyanlilith1992@gmail.com>
2026-06-15 20:24:34 +0300

    feat: Added CRUD for Author

--- library/views.py
+++ library/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from library.models import Author
+from library.serializers import AuthorSerializer
+
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    """CRUD для авторов книг"""
+
+    serializer_class = AuthorSerializer
+    queryset = Author.objects.all().order_by("pk")
