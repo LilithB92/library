2fbce054f8e838320a22e324c224c6957a1c9286
Lilith <bichakhchyanlilith1992@gmail.com>
2026-06-15 20:42:11 +0300

    config(urls.py):Register library's urls in config.urls.py

--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,7 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("users/", include("users.urls", namespace="users")),
+    path("library/", include("library.urls", namespace="library")),
 ]
 
 if settings.DEBUG:
