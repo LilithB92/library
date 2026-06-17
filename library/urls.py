61e8a03a0a01bf0350aa8cdcf2ae094d0c7ac866
Lilith <bichakhchyanlilith1992@gmail.com>
2026-06-17 16:23:21 +0300

    feat(urls.py): Added endpoints for CRUD books

--- library/urls.py
+++ library/urls.py
@@ -1,12 +1,13 @@
 from rest_framework import routers
 
 from library.apps import LibraryConfig
-from library.views import AuthorViewSet
+from library.views import AuthorViewSet, BookViewSet
 
 app_name = LibraryConfig.name
 
 router = routers.SimpleRouter()
 router.register(r"author", AuthorViewSet, basename="author")
+router.register(r"book", BookViewSet, basename="book")
 
 
 urlpatterns = []
