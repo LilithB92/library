83815cd8385ffa502244452aeef680b567312c5c
Lilith <bichakhchyanlilith1992@gmail.com>
2026-06-17 16:12:56 +0300

    docs(model.py): Added model Book for library app

--- library/models.py
+++ library/models.py
@@ -3,6 +3,8 @@ from django_countries.fields import CountryField
 
 
 class Author(models.Model):
+    """Сохраняет одну запись об авторах книг, связанную с:model:`Book`"""
+
     full_name = models.CharField(max_length=100, unique=True)
     date_of_birth = models.DateField(blank=True, null=True)
     date_of_death = models.DateField(blank=True, null=True)
@@ -16,3 +18,43 @@ class Author(models.Model):
     class Meta:
         verbose_name = "Автор"
         verbose_name_plural = "Авторы"
+
+
+class Book(models.Model):
+    """Сохраняет одну запись о книгах, связанную с:model:`Author`"""
+
+    title = models.CharField(
+        max_length=200, verbose_name="Заголовка", help_text="Ведите заголовку"
+    )
+    authors = models.ManyToManyField(
+        Author,
+        related_name="books",
+    )
+    inventory_number = models.PositiveIntegerField(
+        unique=True,
+        verbose_name="Инвентарный номер",
+        help_text="Ведите инвентарный номер",
+    )
+    status_choices = [
+        ("available", "Доступна"),
+        ("borrowed", "Выдана"),
+    ]
+    status = models.CharField(
+        max_length=25, choices=status_choices, default="available", verbose_name="Стату"
+    )
+    genre_choices = [
+        ("fiction", "Художественная литература"),
+        ("non-fiction", "Документальная литература"),
+        ("classic", "Классическая литература"),
+    ]
+    genre = models.CharField(max_length=25, choices=genre_choices, verbose_name="Жанр")
+    published_year = models.PositiveSmallIntegerField(verbose_name="Год публикации")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} книга с инвентарным номером: {self.inventory_number}"
+
+    class Meta:
+        verbose_name = "Книга"
+        verbose_name_plural = "Книги"
