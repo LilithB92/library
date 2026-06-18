b6af23718c3fec93b23441bc47c143495b4532c5
Lilith <bichakhchyanlilith1992@gmail.com>
2026-06-18 22:05:14 +0300

    config: Set drf_spectacular urls

--- config/urls.py
+++ config/urls.py
@@ -2,8 +2,22 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import (SpectacularAPIView, SpectacularRedocView,
+                                   SpectacularSwaggerView)
 
 urlpatterns = [
+    # Выгрузка схемы в формате YAML/JSON
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    # Swagger UI
+    path(
+        "api/docs/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    # ReDoc
+    path(
+        "api/docs/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"
+    ),
     path("admin/", admin.site.urls),
     path("users/", include("users.urls", namespace="users")),
     path("library/", include("library.urls", namespace="library")),
