8b7cc14568fa8b0f41d585173c0dbf9849b894c9
Lilith <bichakhchyanlilith1992@gmail.com>
2026-06-18 21:01:14 +0300

    docs(urls.py): Added return_book endpoint for view ReturnBookAPIView

--- library/urls.py
+++ library/urls.py
@@ -3,7 +3,7 @@ from rest_framework import routers
 
 from library.apps import LibraryConfig
 from library.views import (AuthorViewSet, BookViewSet, BorrowBookApiView,
-                           BorrowBookListAPIView)
+                           BorrowBookListAPIView, ReturnBookAPIView)
 
 app_name = LibraryConfig.name
 
@@ -14,6 +14,7 @@ router.register(r"book", BookViewSet, basename="book")
 
 urlpatterns = [
     path("borrow/<int:pk>/book/", BorrowBookApiView.as_view(), name="borrow_book"),
+    path("return/<int:pk>/book/", ReturnBookAPIView.as_view(), name="return_book"),
     path(
         "borrow/records/", BorrowBookListAPIView.as_view(), name="list_borrowed_records"
     ),
