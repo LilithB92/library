0ebb74bff31333956456d32e2f57dc48dd6fade8
Lilith <bichakhchyanlilith1992@gmail.com>
2026-06-18 18:42:07 +0300

    docs(serializers.py): Added BorrowRecordSerializer for recording borrowed books

--- library/serializers.py
+++ library/serializers.py
@@ -2,7 +2,7 @@ from datetime import date
 
 from rest_framework import serializers
 
-from library.models import Author, Book
+from library.models import Author, Book, BorrowRecord
 
 
 class AuthorSerializer(serializers.ModelSerializer):
@@ -37,6 +37,7 @@ class BookSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Book
+        # authors = AuthorSerializer(many=True, read_only=True)
         fields = (
             "pk",
             "title",
@@ -48,13 +49,14 @@ class BookSerializer(serializers.ModelSerializer):
             "created_at",
             "updated_at",
         )
+        read_only_fields = ["created_at", "updated_at"]
 
     def validate(self, data):
         """Валидация атрибутов(data:{get}) модели Книг"""
         # 1. Проверка: У книги есть автор
         if not data.get("authors"):
             raise serializers.ValidationError("Книга не может быть без автора.")
-        # 1. Проверка: год публикации
+        # 2. Проверка: год публикации
         current_year = date.today().year
         if (data.get("published_year") > current_year) or (
             data.get("published_year") < 868
@@ -63,3 +65,26 @@ class BookSerializer(serializers.ModelSerializer):
                 "Год публикации не может быть меньше чем год публикации первой книги(868) и не больше текущего года."
             )
         return data
+
+
+class BorrowRecordSerializer(serializers.ModelSerializer):
+    """
+    Сериалайзер для модели «Запись выдачи».
+
+    Обрабатывает преобразование экземпляров «Запись выдачи» в формат JSON и выполняет валидацию
+    входящие данные для создания или обновления записи.
+    """
+
+    class Meta:
+        model = BorrowRecord
+        fields = [
+            "pk",
+            "book",
+            "user",
+            "borrow_date",
+            "due_date",
+            "return_date",
+            "is_returned",
+        ]
+        read_only_fields = ["due_date", "borrow_date", "is_returned"]
+        extra_kwargs = {"book": {"required": False}, "user": {"required": False}}
