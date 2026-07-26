0b88b8738e26a12230e1a62b715a5d40d1cda0d4
Lilith <bichakhchyanlilith1992@gmail.com>
2026-07-26 19:28:36 +0300

    feat: add email notifications for borrowing books

--- library/services.py
+++ library/services.py
@@ -2,32 +2,84 @@ from django.core.mail import send_mail
 
 from config import settings
 
+BORROW_SUBJECT = "Выдача книги"
+BORROW_MESSAGE = (
+    "Здравствуйте!\n\n"
+    "Книга «{book_title}» выдана вам до {due_date}.\n"
+    "Пожалуйста, верните книгу вовремя.\n\n"
+    "С уважением,\nБиблиотека"
+)
 
-def send_email_to_readers(book_borrower, book_returner, book_id, request):
-    """
-    Обеспечивает обмен книг читателями с почтой
-    """
-    return_url = f"{request.get_host()}/library/return/{book_id}/book/"
-    borrow_url = f"{request.get_host()}/library/borrow/{book_id}/book/"
-    reader_emails = [book_borrower, book_returner]
-    for email in reader_emails:
-        if email == book_returner:
-            subject = "Возврат книг"
-            message = (
-                f"Здравствуйте! Если Вы хотите сдать книгу свяжитесь с почтой({book_borrower}). "
-                f"Если Вы оба согласны авторизуйтесь и откройте ссылку: {return_url}"
-            )
-        else:
-            subject = "Выдача книг"
-            message = (
-                f"Здравствуйте! Если Вы хотите брать книгу свяжитесь с почтой({book_returner}). "
-                f"Если Вы оба согласны авторизуйтесь и откройте ссылку: {borrow_url}"
-            )
+RETURN_SUBJECT = "Возврат книги"
+RETURN_MESSAGE = (
+    "Здравствуйте!\n\n"
+    "Книга «{book_title}» успешно возвращена.\n"
+    "Спасибо, что пользуетесь библиотекой!\n\n"
+    "С уважением,\nБиблиотека"
+)
 
-        send_mail(
-            subject=subject,
-            message=message,
-            from_email=settings.EMAIL_HOST_USER,
-            recipient_list=[email],  # Список адресов
-            fail_silently=False,
-        )
+OVERDUE_SUBJECT = "Просрочка возврата книги"
+OVERDUE_MESSAGE = (
+    "Здравствуйте!\n\n"
+    "Срок возврата книги «{book_title}» истёк {due_date}.\n"
+    "Пожалуйста, верните книгу как можно скорее.\n\n"
+    "С уважением,\nБиблиотека"
+)
+
+ALREADY_BORROWED_SUBJECT = "Книга занята"
+ALREADY_BORROWED_MESSAGE = (
+    "Здравствуйте!\n\n"
+    "Вы пытаетесь взять книгу «{book_title}», но она уже выдана другому читателю.\n"
+    "Срок возврата: {due_date}.\n\n"
+    "С уважением,\nБиблиотека"
+)
+
+
+def send_borrow_email(user_email, book_title, due_date):
+    """Отправляет email при выдаче книги."""
+    send_mail(
+        subject=BORROW_SUBJECT,
+        message=BORROW_MESSAGE.format(
+            book_title=book_title, due_date=due_date.strftime("%d.%m.%Y")
+        ),
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=[user_email],
+        fail_silently=True,
+    )
+
+
+def send_return_email(user_email, book_title):
+    """Отправляет email при возврате книги."""
+    send_mail(
+        subject=RETURN_SUBJECT,
+        message=RETURN_MESSAGE.format(book_title=book_title),
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=[user_email],
+        fail_silently=True,
+    )
+
+
+def send_overdue_email(user_email, book_title, due_date):
+    """Отправляет email при просрочке."""
+    send_mail(
+        subject=OVERDUE_SUBJECT,
+        message=OVERDUE_MESSAGE.format(
+            book_title=book_title, due_date=due_date.strftime("%d.%m.%Y")
+        ),
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=[user_email],
+        fail_silently=True,
+    )
+
+
+def send_already_borrowed_email(user_email, book_title, due_date):
+    """Уведомляет, что книга уже занята."""
+    send_mail(
+        subject=ALREADY_BORROWED_SUBJECT,
+        message=ALREADY_BORROWED_MESSAGE.format(
+            book_title=book_title, due_date=due_date.strftime("%d.%m.%Y")
+        ),
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=[user_email],
+        fail_silently=True,
+    )
