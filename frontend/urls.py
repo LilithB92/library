8e8f1511b73bc34cba029e392afae8c15c178be2
Lilith <bichakhchyanlilith1992@gmail.com>
2026-07-28 13:49:47 +0300

    auth: add endpoints for auth

--- frontend/urls.py
+++ frontend/urls.py
@@ -13,4 +13,9 @@ urlpatterns = [
     path("authors/create/", views.AuthorCreateView.as_view(), name="author_create"),
     path("authors/<int:pk>/", views.AuthorDetailView.as_view(), name="author_detail"),
     path("authors/<int:pk>/delete/", views.AuthorDeleteView.as_view(), name="author_delete"),
+    path("login/", views.LoginView.as_view(), name="login"),
+    path("register/", views.RegisterView.as_view(), name="register"),
+    path("verify/<str:token>/", views.VerifyView.as_view(), name="verify"),
+    path("profile/", views.ProfileView.as_view(), name="profile"),
+    path("logout/", views.LogoutView.as_view(), name="logout"),
 ]
