129a8f72e4ff0de0d57ded0ea18cd5609df7b71d
Lilith <bichakhchyanlilith1992@gmail.com>
2026-07-28 11:37:02 +0300

    feat: added endpoints for books

--- frontend/urls.py
+++ frontend/urls.py
@@ -6,4 +6,7 @@ app_name = "frontend"
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
+    path("books/create/", views.BookCreateView.as_view(), name="book_create"),
+    path("books/<int:pk>/", views.BookDetailView.as_view(), name="book_detail"),
+    path("books/<int:pk>/delete/", views.BookDeleteView.as_view(), name="book_delete"),
 ]
